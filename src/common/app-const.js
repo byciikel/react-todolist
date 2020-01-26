6bb92bc46a3ffb09bdbd588a2c4d74fc3e6d24f7
Bayu Utomo <by.ciikel@gmail.com>
2020-01-26 09:58:49 +0700

    Add entity todo on app-const

--- src/common/app-const.js
+++ src/common/app-const.js
@@ -8,7 +8,8 @@ export const INITIAL_STATE = {
   entities: {
     [ENTITY_KEY.FOO]: {},
     [ENTITY_KEY.BAR]: {},
-    [ENTITY_KEY.BAZ]: {}
+    [ENTITY_KEY.BAZ]: {},
+    [ENTITY_KEY.TODO]: {}
 
   },
   counter: 0
