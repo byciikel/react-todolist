bd2bffe530414fb0eafb63f2769b67ada47a94d2
Bayu Utomo <by.ciikel@gmail.com>
2020-01-26 22:58:59 +0700

    Update UI: list selected

--- src/components/TodoApp/components/TaskList.jsx
+++ src/components/TodoApp/components/TaskList.jsx
@@ -1,35 +1,54 @@
-import React, { Fragment } from 'react';
+import React, { Component } from 'react';
 import Icon from '../../common/Icon';
 
-const TaskList = ({ todos, state }) => {
-  if (!todos) {
-    return null;
+export class TaskList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: ''
+    }
   }
 
-  const { error, isFetching, data } = todos
-
-  if (isFetching) {
-    return <progress className="progress is-small is-primary" max="100">10%</progress>
-  } else if (error) {
-    return <span>{ error.message }</span>
+  isActive(id) {
+    if (this.state.id === id) {
+      this.setState({ id: '' })
+    } else {
+      this.setState({ id: id })
+    }
   }
 
-  const filterTodos = () => {
-    let stateNumber = state === "doing" ? 0 : 1
-    return data.filter(todo => todo.status === stateNumber)
-  }
+  render() {
+    let { todos, state } = this.props
 
-  return (
-    <ul className="menu-list">
-      {filterTodos().map((todo, index) => (
-        <li key={index} className="subtitle">
-          <a>
-            {todo.title}
-          </a>
-        </li>
-      ))}
-    </ul>
-  )
+    if (!todos) {
+      return null;
+    }
+  
+    const { error, isFetching, data } = todos
+  
+    if (isFetching) {
+      return <progress className="progress is-small is-primary" max="100">10%</progress>
+    } else if (error) {
+      return <span>{ error.message }</span>
+    }
+  
+    const filterTodos = () => {
+      let stateNumber = state === "doing" ? 0 : 1
+      return data.filter(todo => todo.status === stateNumber)
+    }
+  
+    return (
+      <ul className="menu-list">
+        {filterTodos().map((todo, index) => (
+          <li key={index} className="subtitle">
+            <a className={this.state.id === todo.id ? 'is-active' : ''} onClick={() => this.isActive(todo.id)}>
+              {todo.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    )
+  }
 }
 
 export default TaskList
