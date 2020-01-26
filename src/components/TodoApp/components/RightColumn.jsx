74c004424902d090c730b1292f2467d6727e3b69
Bayu Utomo <by.ciikel@gmail.com>
2020-01-26 17:15:17 +0700

    Add done list

--- src/components/TodoApp/components/RightColumn.jsx
+++ src/components/TodoApp/components/RightColumn.jsx
@@ -1,29 +1,57 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
+import { connect } from 'react-redux'
+import { toggleModalThunk } from '../../../redux/actions/todo-modal-thunk'
+import { loadTodos } from '../../../redux/actions/todo-thunk'
+
 import Icon from '../../common/Icon';
 import TodoList from './TodoList'
 import TaskList from './TaskList'
 
-const RightColumn = ({
+class RightColumn extends Component {
+  componentDidMount() {
+    this.props.loadTodos()
+  }
 
-}) => {
-  return (
-    <div className="column">
-      <TodoList
-        title={(
-          <div>
-            <Icon icon="check-circle" className="has-text-info"/>&nbsp;
-            Done List
-          </div>
-        )}
-        subtitle={(
-          <span>
-              List of todos that has been done
-          </span>
-        )}
-        body={<TaskList/>}
-      />
-    </div>
-  )
+  render() {
+    console.log('todos', this.props.todos)
+    return (
+      <div className="column">
+        <TodoList
+          title={(
+            <div className="columns">
+              <div className="column">
+                <Icon icon="list-alt" className="has-text-info"/>&nbsp;
+                Done List
+              </div>
+            </div>
+          )}
+          subtitle={(
+            <span>
+                List of todos that has been done
+            </span>
+          )}
+          body={<TaskList todos={this.props.todos} state="done"/>}
+        />
+      </div>
+    )
+  }
 }
 
-export default RightColumn;
+const mapStateToProps = (state) => {
+  return {
+    isVisible: state.todoModal.isVisible,
+    todos: state.entities && state.entities.todos,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    toggleModal: () => dispatch(toggleModalThunk()),
+    loadTodos: () => dispatch(loadTodos())
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(RightColumn)
